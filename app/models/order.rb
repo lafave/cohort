7ed4a85150f1e433c350c7b4779732991dd4be22
Adam LaFave <lafave.adam@gmail.com>
2014-07-05 16:38:38 -0700

    include Elasticsearch::Model in Order model - allows all orders to be bulk inserted into elasticsearch via Order.import method - override #as_indexed_json to make sure OrderSerializer is used to serialize orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ActiveRecord::Base
+  include Elasticsearch::Model
+
   # Relationships
   belongs_to :user
+
+  def as_indexed_json(opts = {})
+    OrderSerializer.new(self).as_json
+  end
 end
