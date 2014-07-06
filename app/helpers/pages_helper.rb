50472b4767174c55df26dc23c07d9ab734075179
Adam LaFave <lafave.adam@gmail.com>
2014-07-05 17:50:27 -0700

    improve styling

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,9 +1,9 @@
 module PagesHelper
-  def formatted_order_interval(interval, total)
-    total_percentage      = number_to_percentage(interval.total / total.to_f * 100, precision: 1)
-    first_time_percentage = number_to_percentage(interval.first_time_orders / total.to_f * 100, precision: 1)
+  def formatted_order_interval(interval, cohort_total)
+    total_percentage      = number_to_percentage(interval.total / cohort_total.to_f * 100, precision: 1)
+    first_time_percentage = number_to_percentage(interval.first_time_orders / cohort_total.to_f * 100, precision: 1)
 
-    content_tag :td do
+    content_tag :td, :class => "order-interval" do
       @content  = content_tag(:div, "#{total_percentage} orderers (#{interval.total})")
       @content << content_tag(:div, "#{first_time_percentage} 1st time (#{interval.first_time_orders})")
     end
