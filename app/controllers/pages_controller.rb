671dc9e700de816e9d7fca9aa932a8bd47f85350
Adam LaFave <lafave.adam@gmail.com>
2014-07-05 18:14:52 -0700

    fix case of non-integer cohorts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,9 +11,20 @@ class PagesController < ApplicationController
     index_params = params.permit(:cohort_count).symbolize_keys
 
     if index_params[:cohort_count]
-      index_params[:cohort_count] = Integer(index_params[:cohort_count])
+      index_params[:cohort_count] = int_param(:cohort_count, 0)
     end
 
     index_params
+  end
+
+  # @param name [String, Symbol] the name of the param to coerce to an Integer.
+  # @param default [Integer, NilClass] a default value to return if there is an
+  #   error parsing the param.
+  #
+  # @return []
+  def int_param(name, default = nil)
+    Integer(params[name])
+  rescue ArgumentError, TypeError
+    default
   end
 end
