7956eb52796700a82b464db49c45200b0591efbf
Adam LaFave <lafave.adam@gmail.com>
2014-07-05 17:28:53 -0700

    allow cohort_count to be set by param in PagesController#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
 
   def index
-    @results = UserCohortQuery.execute
+    @results = UserCohortQuery.execute index_params
+  end
+
+  private
+
+  # @return [Hash]
+  def index_params
+    index_params = params.permit(:cohort_count).symbolize_keys
+
+    if index_params[:cohort_count]
+      index_params[:cohort_count] = Integer(index_params[:cohort_count])
+    end
+
+    index_params
   end
 end
