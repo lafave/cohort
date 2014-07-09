4801e7b986389b43af4ccd0e4a469b0f0c174159
Adam LaFave <lafave.adam@gmail.com>
2014-07-08 20:20:19 -0700

    set up user model to be imported into elasticsearch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  include Elasticsearch::Model
+
   # Relationships
   has_many :orders
+
+  def as_indexed_json(opts = {})
+    UserSerializer.new(self).as_json
+  end
 end
