18374fc77060edc5c3faea6b6e8c6e5ad2634a57
Adam LaFave <lafave.adam@gmail.com>
2014-07-08 18:23:41 -0700

    update seeds.rb to import data from .csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "csv"
+
+date_time_format = "%m/%d/%Y %H:%M:%S"
+
+# Import users
+puts "importing users..."
+CSV.read("#{Dir.pwd}/db/users.csv", headers: true).each do |user|
+  User.create! \
+    :created_at => DateTime.strptime(user["created_at"], date_time_format),
+    :id         => user["id"],
+    :updated_at => DateTime.strptime(user["updated_at"], date_time_format)
+end
+
+# Import orders
+puts "importing orders..."
+CSV.read("#{Dir.pwd}/db/orders.csv", headers: true).each do |order|
+  Order.create! \
+    :created_at => DateTime.strptime(order["created_at"], date_time_format),
+    :id         => order["id"],
+    :order_num  => order["order_num"],
+    :updated_at => DateTime.strptime(order["updated_at"], date_time_format),
+    :user_id    => order["user_id"]
+end
